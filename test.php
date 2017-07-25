95e21c12ec3f35bef7ab07cb867ff804b2f88ca4
Adaiti Kadams <adaiti@outlook.com>
2017-07-25 16:56:39 +0100

    Added a Banann
    This is just to test my github

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>First Program</title>
+	</head>
+	<body>
+		<p>
+		<?php
+/*			$fuel = -1;
+			echo $fuel <= 1 ? "Fill tank now" : "There's enough fuel";	//This is an Output like the "if" statement
+			echo "<br>";
+			$number = 12345 * 67890;
+			echo substr($number, 3, 3);	
+			echo "</br>";
+			$one = 45*(4*(2+$number)-20);
+			echo "This number is cool:" . $one;*/
+			$con = 1;
+			while ($con <= 10){
+				echo $con ." - Adaiti is a Cool Dude </br>";
+				$con += 1;
+			}
+		?>
+		</p>
+	</body>
+</html>
